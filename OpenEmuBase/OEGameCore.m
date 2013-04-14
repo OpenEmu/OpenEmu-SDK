39a882c51cd170b5b8327c4589c64df5c9af3d96
PGGB <dnagel@outlook.com>
2013-04-14 18:53:34 +0200

    Apply fix for OpenEmu/OpenEmu#539 again

--- OpenEmuBase/OEGameCore.m
+++ OpenEmuBase/OEGameCore.m
@@ -153,7 +153,7 @@ static NSTimeInterval defaultTimeInterval = 60.0;
 - (void)frameRefreshThread:(id)anArgument
 {
     gameInterval = 1./[self frameInterval];
-    NSTimeInterval gameTime = OEMonotonicTime();
+    NSTimeInterval gameTime;
 
     frameFinished = YES;
     willSkipFrame = NO;
@@ -170,7 +170,7 @@ static NSTimeInterval defaultTimeInterval = 60.0;
 
     while(!shouldStop)
     {
-        gameTime += gameInterval;
+        gameTime = OEMonotonicTime() + gameInterval;
         @autoreleasepool
         {
 #if 0
