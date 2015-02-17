1dbd6769d54c2a839bbaef8de33563bb852d7f7c
Kyle Lacy <kylewlacy@me.com>
2015-02-17 10:48:27 -0800

    Add `OEDiffQueue`
    Represents a queue of `NSData`s, but only stores the XOR'd diffs
    between each object

---
+++ OpenEmuBase/OEDiffQueue.mm
@@ -0,0 +1,158 @@
+/*
+ Copyright (c) 2015, OpenEmu Team
+ 
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the OpenEmu Team nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <vector>
+#import "OEDiffQueue.h"
+
+struct OEDiffData
+{
+    unsigned long offset;
+    char diff;
+};
+
+struct OEPatch
+{
+    std::vector<OEDiffData> diffs;
+    size_t length;
+};
+
+@implementation OEDiffQueue
+{
+    char *_currentBytes;
+    size_t _currentLength;
+    std::vector<std::unique_ptr<OEPatch> > _patches;
+    NSUInteger _capacity;
+}
+
+- (id)init
+{
+    if((self = [super init]))
+    {
+        _currentBytes = NULL;
+        _currentLength = 0;
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    free(_currentBytes);
+}
+
+- (void)push:(NSData *)aData
+{
+    char const *nextBytes = (char const *)[aData bytes];
+    size_t nextLength = [aData length];
+    
+    if(_currentBytes)
+    {
+        OEPatch *nextPatch = new OEPatch();
+        nextPatch->length = _currentLength;
+        
+        // `realloc(ptr, 0)` is undefined in C99/C11/C++11, and
+        // may return NULL, or free the memory of `ptr`
+        size_t nextSize = (nextLength > 0) ? nextLength : 1;
+        _currentBytes = (char *)realloc(_currentBytes, nextSize);
+        
+        for(NSUInteger offset = 0; offset < nextLength; ++offset)
+        {
+            char currentPoint = 0;
+            if(offset < _currentLength)
+            {
+                currentPoint = _currentBytes[offset];
+            }
+            
+            char nextPoint = nextBytes[offset];
+            char diffPoint = currentPoint ^ nextPoint;
+            
+            if(diffPoint)
+            {
+                OEDiffData diff = (OEDiffData){
+                    offset, diffPoint
+                };
+                nextPatch->diffs.push_back(diff);
+            }
+            
+            _currentBytes[offset] = nextPoint;
+        }
+        
+        _currentLength = nextLength;
+        _patches.push_back(std::unique_ptr<OEPatch>(nextPatch));
+    }
+    else
+    {
+        _currentLength = nextLength;
+        _currentBytes = (char *)malloc(_currentLength);
+        memcpy((void *)_currentBytes, (void *)nextBytes, _currentLength);
+    }
+}
+
+- (NSData *)pop
+{
+    if([self isEmpty])
+    {
+        NSLog(@"Nothing to pop");
+        return nil;
+    }
+    
+    NSData *popData = [NSData dataWithBytes:_currentBytes length:_currentLength];
+    
+    if(_patches.empty())
+    {
+        NSLog(@"No more patches");
+        free(_currentBytes);
+        _currentBytes = NULL;
+        _currentLength = 0;
+    }
+    else
+    {
+        OEPatch *patch = _patches.back().get();
+        _currentBytes = (char *)realloc(_currentBytes, patch->length);
+        _currentLength = patch->length;
+        
+        for(auto itr = patch->diffs.begin(); itr < patch->diffs.end(); ++itr)
+        {
+            OEDiffData diff = *itr;
+            
+            if(diff.offset < _currentLength)
+            {
+                _currentBytes[diff.offset] ^= diff.diff;
+            }
+        }
+        
+        _patches.pop_back();
+    }
+    
+    return popData;
+}
+
+- (BOOL)isEmpty
+{
+    return _currentBytes == NULL;
+}
+
+@end
