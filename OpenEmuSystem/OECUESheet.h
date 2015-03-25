bd8271d62f0bcccf22f36081d2eb84bae410e434
clobber <brymaster@gmail.com>
2015-03-25 15:19:13 -0500

    Disallow import of broken cue sheets
    Shouldn't use unless we repair the files at import, otherwise the emulators won't boot these anyway

--- OpenEmuSystem/OECUESheet.h
+++ OpenEmuSystem/OECUESheet.h
@@ -27,8 +27,8 @@
 #import <Foundation/Foundation.h>
 
 // Options to handle 'broken' cue files:
-#define OECUESheetImproveReadingByUsingBinExtension 1 // If a referenced file does not have an extension we automatically append .bin and see if that works better
-#define OECUESheetImproveReadingByUsingSheetBin 1 // if there's only one referenced file which is not available we check if there's a bin file with the same name as the cue and use that instead
+#define OECUESheetImproveReadingByUsingBinExtension 0 // If a referenced file does not have an extension we automatically append .bin and see if that works better
+#define OECUESheetImproveReadingByUsingSheetBin 0 // if there's only one referenced file which is not available we check if there's a bin file with the same name as the cue and use that instead
 
 /**
  Class to handle .cue files and manage track files references in cue sheets.
