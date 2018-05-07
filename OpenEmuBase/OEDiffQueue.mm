e73e03c557b9b7980c7cd6643a19a6d5c40405eb
Daniele Cattaneo <daniele.cattaneo@alice.it>
2018-05-07 19:50:08 +0200

    Improve performance of OEDiffQueue.
    In specific cases, when the save state is big, pushing a state could take as much time as running the emulation itself.

--- OpenEmuBase/OEDiffQueue.mm
+++ OpenEmuBase/OEDiffQueue.mm
@@ -31,20 +31,41 @@
 
 struct OEDiffData
 {
-    unsigned long offset;
-    char diff;
+    uint32_t offset; /* must always be aligned to a 4 byte boundary */
+    uint32_t delta;  /* bytes to overwrite */
+};
+
+struct OEDeltaPatch
+{
+    /* Array of changes to make to reconstruct the previous data.
+     * The diff only applies to the bytes in common, i.e. if we are
+     * reconstructing a 3 KB NSData from a 2 KB NSData we will diff only
+     * the first 2 KB. The remaining 1 KB is stored as a flat blob called
+     * overflow. To save memory and calls to malloc, the overflow is stored
+     * in the items array, after the OEDiffDatas. */
+    OEDiffData *items = nullptr;
+    
+    uint32_t itemCount;      /* # diff data items, overflow blob starts at &items[itemCount] */
+    uint32_t overflowOffset; /* offset of the overflow in the reconstructed data */
+    uint32_t overflowLength;
+    size_t unpackedLength;   /* size of the reconstructed data */
 };
 
 struct OEPatch
 {
-    std::vector<OEDiffData> diffs;
-    size_t length;
+    OEDeltaPatch deltaPatch;
+    NSData *fallback;
+    
+    OEPatch(): fallback(nil) {}
+    OEPatch(OEPatch& p) = delete;
+    ~OEPatch() {
+        free(deltaPatch.items);
+    }
 };
 
 @implementation OEDiffQueue
 {
-    char *_currentBytes;
-    size_t _currentLength;
+    NSData *_currentData;
     std::deque<std::unique_ptr<OEPatch> > _patches;
     NSUInteger _capacity;
 }
@@ -58,108 +79,130 @@ struct OEPatch
 {
     if((self = [super init]))
     {
-        _currentBytes = NULL;
-        _currentLength = 0;
+        _currentData = nil;
         _capacity = MAX(capacity, 2);
         // Note: A capacity <2 crashes in [OEDiffQueue push:]
     }
     return self;
 }
 
-- (void)dealloc
-{
-    free(_currentBytes);
-}
-
 - (void)push:(NSData *)aData
 {
-    char const *nextBytes = (char const *)[aData bytes];
+    if (!_currentData) {
+        _currentData = aData;
+        return;
+    }
+    
+    OEPatch *newPatch = new OEPatch();
+    if (![self _generateDeltaPatch:newPatch->deltaPatch fromData:aData])
+        newPatch->fallback = _currentData;
+    
+    _currentData = aData;
+    
+    if ([self count] >= _capacity) {
+        NSUInteger discrepancy = [self count] - _capacity + 1;
+        _patches.erase(_patches.begin(), _patches.begin() + discrepancy);
+    }
+    
+    _patches.push_back(std::unique_ptr<OEPatch>(newPatch));
+}
+
+- (BOOL)_generateDeltaPatch:(OEDeltaPatch&)delta fromData:(NSData *)aData
+{
+    if ([aData length] >= UINT32_MAX)
+        return NO;
+    
+    const char *nextBytes = (const char *)[aData bytes];
+    const uint32_t *nextLongs = (const uint32_t *)nextBytes;
     size_t nextLength = [aData length];
     
-    if(_currentBytes)
-    {
-        OEPatch *nextPatch = new OEPatch();
-        nextPatch->length = _currentLength;
-        
-        // `realloc(ptr, 0)` is undefined in C99/C11/C++11, and
-        // may return NULL, or free the memory of `ptr`
-        size_t nextSize = MAX(nextLength, 1);
-        _currentBytes = (char *)realloc(_currentBytes, nextSize);
-        
-        for(NSUInteger offset = 0; offset < nextLength; ++offset)
-        {
-            char currentPoint = 0;
-            if(offset < _currentLength)
-            {
-                currentPoint = _currentBytes[offset];
-            }
-            
-            char nextPoint = nextBytes[offset];
-            char diffPoint = currentPoint ^ nextPoint;
-            
-            if(diffPoint)
-            {
-                OEDiffData diff = (OEDiffData){
-                    offset, diffPoint
-                };
-                nextPatch->diffs.push_back(diff);
-            }
-            
-            _currentBytes[offset] = nextPoint;
+    char const *currentBytes = (const char *)[_currentData bytes];
+    const uint32_t *currentLongs = (const uint32_t *)currentBytes;
+    size_t currentLength = [_currentData length];
+    delta.unpackedLength = currentLength;
+    
+    /* the maximum number of diff data items is set to the break-even point
+     * between using the diff and storing the original NSData directly. */
+    size_t maxItems = nextLength / sizeof(OEDiffData);
+    delta.items = (OEDiffData *)malloc((maxItems + 1) * sizeof(OEDiffData));
+    
+    size_t deltaDataLimit = MIN(nextLength, currentLength) / sizeof(uint32_t);
+    size_t deltai, datai;
+    size_t realDiffLength;
+    for (datai = 0, deltai = 0; datai < deltaDataLimit && deltai < maxItems; datai++) {
+        uint32_t nextDelta = currentLongs[datai];
+        if (nextLongs[datai] != nextDelta) {
+            delta.items[deltai].delta = nextDelta;
+            delta.items[deltai].offset = datai * sizeof(uint32_t);
+            deltai++;
         }
-        
-        _currentLength = nextLength;
-        
-        if([self count] >= _capacity)
-        {
-            NSUInteger discrepancy = [self count] - _capacity + 1;
-            _patches.erase(_patches.begin(), _patches.begin() + discrepancy);
-        }
-        _patches.push_back(std::unique_ptr<OEPatch>(nextPatch));
     }
-    else
-    {
-        _currentLength = nextLength;
-        _currentBytes = (char *)malloc(_currentLength);
-        memcpy((void *)_currentBytes, (void *)nextBytes, _currentLength);
+    delta.itemCount = deltai;
+    
+    if (deltai >= maxItems)
+        goto fail;
+    
+    if (datai * sizeof(uint32_t) < currentLength) {
+        delta.overflowLength = currentLength - datai * sizeof(uint32_t);
+        delta.overflowOffset = datai * sizeof(uint32_t);
+        if ((maxItems - deltai) * sizeof(OEDiffData) <= delta.overflowLength)
+            goto fail;
+        
+        char *overflowStart = (char *)(delta.items + deltai);
+        memcpy(overflowStart, &currentLongs[datai], delta.overflowLength);
+    } else {
+        delta.overflowLength = 0;
     }
+    
+    realDiffLength = delta.itemCount * sizeof(OEDiffData) + delta.overflowLength;
+    if (void *newbuf = realloc(delta.items, realDiffLength))
+        delta.items = (OEDiffData *)newbuf;
+    
+    return YES;
+    
+fail:
+    free(delta.items);
+    delta.items = nullptr;
+    return NO;
 }
 
 - (NSData *)pop
 {
-    if([self isEmpty])
-    {
-        return nil;
-    }
+    NSData *prev = _currentData;
     
-    NSData *popData = [NSData dataWithBytes:_currentBytes length:_currentLength];
-    
-    if(_patches.empty())
-    {
-        free(_currentBytes);
-        _currentBytes = NULL;
-        _currentLength = 0;
-    }
-    else
-    {
+    if (_patches.size() > 0) {
         OEPatch *patch = _patches.back().get();
-        _currentBytes = (char *)realloc(_currentBytes, patch->length);
-        _currentLength = patch->length;
-        
-        for(auto itr = patch->diffs.begin(); itr < patch->diffs.end(); ++itr)
-        {
-            OEDiffData diff = *itr;
-            
-            if(diff.offset < _currentLength)
-            {
-                _currentBytes[diff.offset] ^= diff.diff;
-            }
-        }
-        
+        if (patch->fallback)
+            _currentData = patch->fallback;
+        else
+            _currentData = [self _reconstructDataFromDeltaPatch:patch->deltaPatch];
         _patches.pop_back();
+    } else {
+        _currentData = nil;
     }
     
-    return popData;
+    return prev;
+}
+
+- (NSData *)_reconstructDataFromDeltaPatch:(OEDeltaPatch&)delta
+{
+    char *buffer = (char *)malloc(delta.unpackedLength);
+    size_t sharedLength = MIN(delta.unpackedLength, _currentData.length);
+    memcpy(buffer, _currentData.bytes, sharedLength);
+    
+    uint32_t *bufferLongs = (uint32_t *)buffer;
+    uint32_t i;
+    for (i = 0; i < delta.itemCount; i++) {
+        OEDiffData *data = delta.items + i;
+        bufferLongs[data->offset / sizeof(uint32_t)] = data->delta;
+    }
+    
+    if (delta.overflowLength > 0) {
+        char *oflstart = buffer + delta.overflowOffset;
+        memcpy(oflstart, &(delta.items[i]), delta.overflowLength);
+    }
+    
+    return [NSData dataWithBytesNoCopy:buffer length:delta.unpackedLength];
 }
 
 - (NSUInteger)count
@@ -174,7 +217,7 @@ struct OEPatch
 
 - (BOOL)isEmpty
 {
-    return _currentBytes == NULL;
+    return _currentData == NULL;
 }
 
 @end
