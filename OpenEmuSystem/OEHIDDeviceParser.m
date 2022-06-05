ae6dd1ff6ca63b6940656f30821a5df2ae0eccb5
Steve Cox <coxsteve@gmail.com>
2022-06-05 11:39:01 -0700

    adds console detail about profile missing buttons

--- OpenEmuSystem/OEHIDDeviceParser.m
+++ OpenEmuSystem/OEHIDDeviceParser.m
@@ -290,7 +290,7 @@ typedef NS_ENUM(NSInteger, OEElementType) {
         NSLog(@"WARNING: There are %ld generic desktop elements unaccounted for in %@. Details: %@", genericDesktopElements.count, controllerDesc.name, genericDesktopElements.description);
 
     if([buttonElements count] > 0)
-        NSLog(@"WARNING: There are %ld button elements unaccounted for in %@. Details: %@", buttonElements.count, controllerDesc.name, genericDesktopElements.description);
+        NSLog(@"WARNING: There are %ld button elements unaccounted for in %@. Details: %@", buttonElements.count, controllerDesc.name, buttonElements.description);
 
     return attributes;
 }
